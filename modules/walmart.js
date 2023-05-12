49193a1999f642ef6b97834a9ececbf636433dd2
Emiro Trechy <e.trechy.p@gmail.com>
2023-05-12 16:19:15 -0400

    Update api params on walmart.js

--- modules/walmart.js
+++ modules/walmart.js
@@ -13,11 +13,10 @@ searchOnWalmart = async () => {
     let rnd = Math.floor(Math.random() * catalogId.length);
     let config = {
         headers: { 'Authorization': `Basic ${Buffer.from(`${process.env.IMPACT_CLIENT_ID}:${process.env.IMPACT_CLIENT_SECRET}`).toString('base64')}` },
-        params: { Query: "CurrentPrice <= 20 AND DiscountPercentage >= 50 AND StockAvailability=InStock" }
+        params: { Query: "CurrentPrice <= 20 AND DiscountPercentage >= 50 AND StockAvailability='InStock'" }
     };
     try {
-        //const response = await axios.get(`https://api.impact.com/Mediapartners/${process.env.IMPACT_CLIENT_ID}/Catalogs/${catalogId[rnd]}/Items?IrVersion=12&PageSize=1000`, config);
-        const response = await axios.get(`https://api.impact.com/Mediapartners/${process.env.IMPACT_CLIENT_ID}/Catalogs/${catalogId[rnd]}/Items?IrVersion=12`, config);
+        const response = await axios.get(`https://api.impact.com/Mediapartners/${process.env.IMPACT_CLIENT_ID}/Catalogs/${catalogId[rnd]}/Items?IrVersion=12&PageSize=1000`, config);
         let random = Math.floor(Math.random() * response.data.Items.length)
         let product = {
             name: response.data.Items[random].Name,
